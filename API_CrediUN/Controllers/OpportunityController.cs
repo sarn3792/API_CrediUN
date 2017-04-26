be557fb3f192b6c771eb1e0c8fb525167df452a2
sarn3792 <sarn3792@gmail.com>
2017-04-26 10:50:16 -0500

    All methods were added in OpportunityController. Also, DataBase class

--- API_CrediUN/Controllers/OpportunityController.cs
+++ API_CrediUN/Controllers/OpportunityController.cs
@@ -12,21 +12,130 @@ namespace API_CrediUN.Controllers
     public class OpportunityController : ApiController
     {
         [HttpGet]
-        [Route("api/Opportunity/GetByContractName/{contractNumber}")]
+        [Route("api/Opportunity/GetByContractNumber/{contractNumber}")]
         public HttpResponseMessage GetByContractNumber(string contractNumber)
         {
             try
             {
                 string result = string.Empty;
-                if(result == string.Empty)
+                if(result != string.Empty)
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, "test");
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
                 }
                 else
                 {
-                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Opportunity with contract number {0} was not found", contractNumber));
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Oportunidad con número de contrato {0} no fue encontrada", contractNumber));
                 }
             } catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Opportunity/GetCutOffDate/{idOpportunity}")]
+        public HttpResponseMessage GetCutOffDate(string idOpportunity)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (result != string.Empty)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, result;
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Fecha de corte para la oportunidad {0} no fue encontrada", idOpportunity));
+                }
+            } catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Opportunity/GetBalance/{idOpportunity}")]
+        public HttpResponseMessage GetBalance(string idOpportunity)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (result != string.Empty)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Saldo al día para la oportunidad {0} no fue encontrado", idOpportunity));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Opportunity/GetNextPaymentDue/{idOpportunity}")]
+        public HttpResponseMessage GetNextPaymentDue(string idOpportunity)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (result != string.Empty)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Mensualidad actual para la oportunidad {0} no fue encontrada", idOpportunity));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Opportunity/GetCurrentMonthly/{idOpportunity}")]
+        public HttpResponseMessage GetCurrentMonthly(string idOpportunity)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (result != string.Empty)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Fecha de corte para la oportunidad {0} no fue encontrada", idOpportunity));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Opportunity/GetOverdueBalance/{idOpportunity}")]
+        public HttpResponseMessage GetOverdueBalance(string idOpportunity)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (result != string.Empty)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, String.Format("Saldo vencido para la oportunidad {0} no fue encontrado", idOpportunity));
+                }
+            }
+            catch (Exception ex)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
